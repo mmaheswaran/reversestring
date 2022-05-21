dc9408f0c35a884c70a9f5dc12732cbbb92fc4fe
Mary-Ann Maheswaran <maryann.thyveetil@gmail.com>
2022-05-21 23:05:34 +0100

    Add recursive and stack methods.

--- ReverseString.cpp
+++ ReverseString.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 #include <chrono>
-#include <math.h>
 #include <vector>
+#include <stack>
 using namespace std;
 
-void first_method(string &word) {
+void stdreverse(const string &word) {
 
     string reword(word);
 
@@ -14,13 +14,13 @@ void first_method(string &word) {
 
 }
 
-void second_method(string &word) {
+void stdswap(const string &word) {
 
     string reword(word);
     int l = word.size();
     int lastindex=word.size()-1;
 
-    for (int i = 0; i < floor(l*0.5); i++) {
+    for (int i = 0; i < l/2; i++) {
         std::swap(reword[i], reword[lastindex-i]);
 
     }
@@ -28,13 +28,13 @@ void second_method(string &word) {
     cout << reword << ' ';
 }
 
-void third_method(string &word) {
+void manualswap(const string &word) {
 
     string reword(word);
     int l = word.size();
     int lastindex=word.size()-1;
 
-    for (int i = 0; i < floor(l*0.5); i++) {
+    for (int i = 0; i < l/2; i++) {
         char tmp = reword[i];
         reword[i] = reword[lastindex-i];
         reword[lastindex-i] = tmp;
@@ -42,6 +42,29 @@ void third_method(string &word) {
     }
     cout << reword << ' ';
 
+}
+
+
+void recursive_swap(string &word, int l, int h) {
+    if (l < h) {
+        swap(word[l], word[h]);
+        recursive_swap(word, l + 1, h - 1);
+    }
+}
+
+void stack_swap(string &word) {
+    string reword(word);
+
+    stack<char> s;
+    for (int i = 0; i <word.length(); i++) {
+        s.push(word[i]);
+    }
+
+    for (int i = 0; i < word.length(); i++) {
+        reword[i] = s.top();
+        s.pop();
+    }
+    cout << reword << ' ';
 }
 
 
@@ -54,36 +77,56 @@ int main(int argc, char **argv) {
     for (auto i = strtok(&words[0], " "); i != NULL; i = strtok(NULL, " "))
         tokens.push_back(i);
 
-    //First method
+    //std::reverse method
     auto start = std::chrono::high_resolution_clock::now();
     for (vector<string>::iterator t=tokens.begin(); t!=tokens.end(); ++t) {
-        first_method(*t);
+        stdreverse(*t);
     }
     auto end = chrono::high_resolution_clock::now();
     auto int_s = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "\nfirst_method() elapsed time is " << int_s.count() << " microseconds" << endl;
+    cout << "stdreverse() elapsed time is " << int_s.count() << " microseconds" << endl;
 
 
-    //Second method
+    //std::swap method
     start = std::chrono::high_resolution_clock::now();
     for (vector<string>::iterator t=tokens.begin(); t!=tokens.end(); ++t) {
-        second_method(*t);
+        stdswap(*t);
     }
     end = chrono::high_resolution_clock::now();
     int_s = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "\nsecond_method() elapsed time is " << int_s.count() << " microseconds" << endl;
+    cout << "stdswap() elapsed time is " << int_s.count() << " microseconds" << endl;
     start = std::chrono::high_resolution_clock::now();
 
-    //Third method
+    //manual swap method
     start = std::chrono::high_resolution_clock::now();
     for (vector<string>::iterator t=tokens.begin(); t!=tokens.end(); ++t) {
-        third_method(*t);
+        manualswap(*t);
     }
     end = chrono::high_resolution_clock::now();
     int_s = chrono::duration_cast<chrono::microseconds>(end - start);
-    cout << "\nthird_method() elapsed time is " << int_s.count() << " microseconds" << endl;
+    cout << "manualswap() elapsed time is " << int_s.count() << " microseconds" << endl;
     start = std::chrono::high_resolution_clock::now();
 
+
+    //recursive swap method
+    start = std::chrono::high_resolution_clock::now();
+    for (vector<string>::iterator t=tokens.begin(); t!=tokens.end(); ++t) {
+        string w = *t;
+        recursive_swap(w, 0, w.length() - 1);
+        cout << w << " ";
+    }
+    end = chrono::high_resolution_clock::now();
+    int_s = chrono::duration_cast<chrono::microseconds>(end - start);
+    cout << "recursive_swap() elapsed time is " << int_s.count() << " microseconds" << endl;
+
+    //stack swap method
+    start = std::chrono::high_resolution_clock::now();
+    for (vector<string>::iterator t=tokens.begin(); t!=tokens.end(); ++t) {
+        stack_swap(*t);
+    }
+    end = chrono::high_resolution_clock::now();
+    int_s = chrono::duration_cast<chrono::microseconds>(end - start);
+    cout << "stack_swap() elapsed time is " << int_s.count() << " microseconds" << endl;
 
     return 0;
 }
